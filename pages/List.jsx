b9fc3edf45b8f7dae2c26a4238d3ed79ad589ad0
SalmaSayed <salmasayed797@gmail.com>
2022-07-27 21:07:17 +0200

    feat: initialize list component and remove boilerplate code

---
+++ pages/List.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const List = () => {
+  return <div>Table</div>;
+};
+
+export default List;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Sakneen</title>
+        <link rel="icon" href="/fav.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
