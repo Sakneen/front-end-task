8a667217aca71d6e07074f60dec0376a211317db
SalmaSayed <salmasayed797@gmail.com>
2022-07-28 04:05:30 +0200

    feat: add filter and sorting design and styles

--- pages/List.js
+++ pages/List.js
@@ -23,6 +23,27 @@ const List = () => {
           <span>&gt;</span>
           <span>Dashboard</span>
         </div>
+        <div className={styles.controllers}>
+          <span>
+            <span className={styles.filter}>Filters by ID:</span>
+            <input className={styles.input} placeholder="ex: 56487" />
+          </span>
+          <span className={styles.sort}>
+            <Image src="/sort.png" alt="sort icon" width={16} height={16} />
+            <span>sort by:</span>
+            <select className={styles.select}>
+              <option value="id" className={styles.option}>
+                Unit ID
+              </option>
+              <option value="type" className={styles.option}>
+                Unit type
+              </option>
+              <option value="price" className={styles.option}>
+                Unit price
+              </option>
+            </select>
+          </span>
+        </div>
       </main>
       <footer>
         <a href="mailto:info@sakneen.com "> Contact us: info@sakneen.com</a>
