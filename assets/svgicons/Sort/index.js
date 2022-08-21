37fe532466ec1dcb1a9befd80f65fdd75f414688
MohamedFouadrn <mfoad@clinico-app.com>
2022-08-21 05:05:40 +0200

    add svg icons

---
+++ assets/svgicons/Sort/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SortIcon = (props) => {
+    return (
+        <div {...props} >
+            <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.5 9.09094H13.5" stroke="#9E9E9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M1.6875 5.71594H16.3125" stroke="#9E9E9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M7.3125 12.4659H10.6875" stroke="#9E9E9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+
+        </div>
+    )
+}
+
+export default SortIcon
+
