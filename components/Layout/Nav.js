c52acae1c53c38861889ffc351f31f796bb362c4
Moussel <moussel2011@gmail.com>
2022-08-25 13:16:35 +0200

    refactor(layout): update components structure

--- components/Layout/Nav.js
+++ components/Layout/Nav.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import navStyles from "../styles/Nav.module.css";
+import navStyles from "../../styles/Nav.module.css";
 
 const Nav = () => {
   return (
