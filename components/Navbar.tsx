94d275ceb4d3907a33a14da5353e7c61f87cbb9e
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-26 07:41:02 +0200

    Add Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,13 @@
+import Image from 'next/image';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-[#2419BE]">
+      <nav className="h-12 container flex items-center shadow-md">
+        <Image src={'/sakneen-logo.svg'} width={25} height={25} />
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
   },
   plugins: [],
 };
