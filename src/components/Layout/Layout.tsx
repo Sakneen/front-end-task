a3ada538122752304d8ac4dce3b770ad40c47736
Soul-Remix <soulremix-dev@pm.me>
2023-03-08 21:12:54 +0200

    Move Head elements from _document to layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { CssBaseline } from "@mui/material";
 import { NavBar, Footer } from "@/components";
 import { ReactElement } from "react";
+import Head from "next/head";
 
 interface Props {
   children: ReactElement;
@@ -9,6 +10,17 @@ interface Props {
 export function Layout({ children }: Props) {
   return (
     <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href="https://www.sakneen.com/assets/website-manifest/favicon-32x32.png"
+        />
+      </Head>
       <CssBaseline />
       <NavBar />
       <main>{children}</main>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="icon"
-          href="https://www.sakneen.com/assets/website-manifest/favicon-32x32.png"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
